133ddcbf94b80508bca127068ca114b9015f531f
Dka-11 <adikaahmad05@gmail.com>
2022-03-10 11:28:45 +0700

    first commit

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -4,19 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class PageController extends Controller
+class AboutController extends Controller
 {
-    public function index(){
-        return "Selamat Datang";
-    }
-
     public function about(){
         return "NIM   : 2041720171 <br> 
                 Nama  : Adika Ahmad Hanif Nazhir <br>
                 Kelas : TI-2B";
-    }
-
-    public function articles($id){
-        return "Halaman Artikel dengan id ".$id;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,4 @@
 <?php
-
-use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,16 +12,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return "Selamat Datang";
-});
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
 
-Route::get('/about', function () {
-    return "NIM   : 20417210171 <br>
-            Nama  : Adika Ahmad Hanif Nazhir <br>
-            Kelas : TI-2B";
-});
+Route::get('/', [HomeController::class,'index']);
 
-Route::get('/articles/{id}', function($id) {
-    return "Halaman artikel dengan ID {$id} ";
-});
+Route::get('/about', [AboutController::class,'about']);
+
+Route::get('/articles/{id}', [ArticleController::class,'articles']);
